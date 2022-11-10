a4d6c6ed861d752fc85d628f07728dcc79f0b143
Rashko Georgiev Dimitrov <rdimitrov@gameaccount.com>
2022-11-10 15:43:45 +0200

    Add Koin

--- app/src/main/java/com/example/composedemo/ComposeDemoNavGraph.kt
+++ app/src/main/java/com/example/composedemo/ComposeDemoNavGraph.kt
@@ -7,15 +7,15 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.composedemo.navigation.Routes
-import com.example.composedemo.ui.fragment.BreedsListFragment
 import com.example.composedemo.ui.fragment.breeddetails.BreedDetailsFragment
+import com.example.composedemo.ui.fragment.breedlist.BreedListFragment
 
 @Composable
 fun ComposeDemoNavGraph() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Routes.BreedsList.route) {
         composable(route = Routes.BreedsList.route) {
-            BreedsListFragment(onListItemClick = { breed ->
+            BreedListFragment(onListItemClick = { breed ->
                 navController.navigate(
                     Routes.BreedDetails.createRoute(breed)
                 )
--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -6,11 +6,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.composedemo.di.viewModelModule
 import com.example.composedemo.ui.theme.ComposeDemoTheme
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startKoin {
+            // Log Koin into Android logger
+            androidLogger()
+            // Reference Android context
+            androidContext(this@MainActivity)
+            // Load modules
+            modules(viewModelModule)
+        }
         setContent {
             ComposeDemoTheme {
                 ComposeDemoNavGraph()
